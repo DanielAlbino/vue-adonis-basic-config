422298239c71fffc4b2e3074e75b5a8ca0c2ecd2
Daniel Albino <daniel.albino@polygon.pt>
2019-08-26 15:09:02 +0100

    Base adonis and artyom.js created and installed

---
+++ resources/assets/js/router/index.js
@@ -0,0 +1,13 @@
+// resources/assets/js/router/index.js
+
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history", // use HTML5 history instead of hashes
+  routes: [
+    // all routes
+  ]
+});
---
+++ resources/assets/js/store/artyom.js
@@ -0,0 +1,5 @@
+// Or if installed from NPM to use with a bundler
+import Artyom from "artyom.js";
+
+// const artyom = require("artyom.js");
+const artyom = new Artyom();
