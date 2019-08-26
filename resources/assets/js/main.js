2a0971e2caa3a40e10b72e26ab3386d3bbbd6bea
Daniel Albino <daniel.albino@polygon.pt>
2019-08-26 15:20:20 +0100

    P1 - greetings commands was added in artyom.js file

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -2,7 +2,7 @@
 
 import Vue from "vue";
 import router from "./router";
-import App from "@/components/layout/App";
+import App from "@/App";
 
 Vue.config.productionTip = false;
 
--- resources/assets/js/store/artyom.js
+++ resources/assets/js/store/artyom.js
@@ -3,3 +3,28 @@ import Artyom from "artyom.js";
 
 // const artyom = require("artyom.js");
 const artyom = new Artyom();
+
+var commands = [
+  {
+    indexes: ["Hello noa", "Good night noa", "Good morning noa"],
+    action: function(i) {
+      switch (i) {
+        case 1:
+          alert("Hi, how are you ?");
+          break;
+        case 2:
+          alert("Good night, how are you ?");
+          break;
+        case 3:
+          alert("Good morning, how are you ?");
+          break;
+      }
+    }
+  }
+];
+
+var noa = artyom.addCommands(commands);
+
+export default {
+  noa
+};
